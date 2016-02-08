0d2683c5fef87e38cda124f7817978e3357c2f26
Zeal Dalal <zeal_dalal@yahoo.co.in>
2016-02-08 12:54:57 -0500

    Create PageController.php

---
+++ Programming Assignment 2/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller
+{
+    //
+	public function index()
+	{
+		return "Hello from index page";
+	}
+	
+	public function greet($name)
+	{
+		return "Hello from".$name;
+	}
+}
